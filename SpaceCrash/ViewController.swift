5087224ae0408aa7ab54fed007ca2d49b98143c6
absimas <ab.simas@gmail.com>
2015-03-27 19:48:51 +0200

    Initial Commit

---
+++ SpaceCrash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceCrash
+//
+//  Created by Simas Abramovas on 27/03/15.
+//  Copyright (c) 2015 Simas Abramovas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
